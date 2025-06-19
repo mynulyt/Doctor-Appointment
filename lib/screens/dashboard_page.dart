dead45b97f90cc7488b6c1232ff3886e9b9fd24e
Mynul Alam <mynulalam27@gmail.com>
2025-06-19 23:25:36 +0600

    commit

--- lib/screens/dashboard_page.dart
+++ lib/screens/dashboard_page.dart
@@ -1,1 +1,26 @@
+import 'package:flutter/material.dart';
+import 'widgets/doctor_card.dart';
 
+class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Doctor Dashboard")),
+      body: ListView(
+        padding: const EdgeInsets.all(20),
+        children: [
+          const Text(
+            'Available Doctors:',
+            style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+          ),
+          const SizedBox(height: 12),
+          DoctorCard(name: 'Dr. Ayesha Rahman', specialty: 'Cardiologist'),
+          DoctorCard(name: 'Dr. Hasan Ahmed', specialty: 'Dermatologist'),
+          DoctorCard(name: 'Dr. Nilufa Sultana', specialty: 'Pediatrician'),
+        ],
+      ),
+    );
+  }
+}

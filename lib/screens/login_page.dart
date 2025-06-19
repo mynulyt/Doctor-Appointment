a9d5a3e3cbf57445ae1dec8aaaaced056603442a
Mynul Alam <mynulalam27@gmail.com>
2025-06-19 23:24:39 +0600

    commit

---
+++ lib/screens/login_page.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+import 'dashboard_page.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Login")),
+      body: Padding(
+        padding: const EdgeInsets.all(20),
+        child: Column(
+          children: [
+            const TextField(decoration: InputDecoration(labelText: 'Email')),
+            const TextField(
+              decoration: InputDecoration(labelText: 'Password'),
+              obscureText: true,
+            ),
+            const SizedBox(height: 24),
+            ElevatedButton(
+              onPressed:
+                  () => Navigator.push(
+                    context,
+                    MaterialPageRoute(builder: (_) => const DashboardPage()),
+                  ),
+              child: const Text('Login'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}

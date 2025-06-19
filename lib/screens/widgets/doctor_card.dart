f7baa4ac174a6acdbe734f162ee1b6235955d54c
Mynul Alam <mynulalam27@gmail.com>
2025-06-19 23:26:23 +0600

    commit

--- lib/screens/widgets/doctor_card.dart
+++ lib/screens/widgets/doctor_card.dart
@@ -1,1 +1,25 @@
+import 'package:flutter/material.dart';
 
+class DoctorCard extends StatelessWidget {
+  final String name;
+  final String specialty;
+  const DoctorCard({required this.name, required this.specialty, super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      elevation: 4,
+      margin: const EdgeInsets.symmetric(vertical: 10),
+      child: ListTile(
+        leading: const CircleAvatar(
+          backgroundColor: Colors.blue,
+          child: Icon(Icons.person, color: Colors.white),
+        ),
+        title: Text(name),
+        subtitle: Text(specialty),
+        trailing: ElevatedButton(onPressed: () {}, child: const Text("Book")),
+      ),
+    );
+  }
+}
